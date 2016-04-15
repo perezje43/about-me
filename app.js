92684da8bb519853c4532cd6ce6cf749ed71a7c7
Jeremy Perez <perezje43@gmail.com>
2016-04-15 11:05:55 -0700

    Added console.log statements to questions 6 and 7

--- app.js
+++ app.js
@@ -1,66 +1,56 @@
 var userName = prompt('Hi! What is your name?');
 alert('Hi ' + userName + '! Let\'s play a guessing game about me...');
+console.log('The user\'s name is: ' + userName);
 
 var totalTally = 0;
 
-var questionsArray = ['Am I from Seattle?',
-'Have I been skydiving before?',
-'My favorite place I\'ve recently visted is DC?',
-'My favorite burger place is In\' N\' Out?',
-'Have I studied abroad in Tahiti?'
-];
-
-var correctResponse = ['Correct! I am from Seattle',
-'Correct! I am planning to!',
-'Correct! My favorite landmark was the Jefferson Memorial.',
-'You\'re right! But there has never been a burger I didn\'t like!',
-'Correct! I have studied abroad in Tahiti!'
-];
-
-var wrongResponse = ['No! I am from Seattle!',
-'No! But I plan to!',
-'Wrong! It was my favorite recent visit!',
-'No! But I really want to try it!',
-'No! I did study abroad in Tahiti.'
-];
-
-var correctAnswers = [['Y', 'YES'], ['N', 'NO'], ['Y', 'YES'], ['N', 'NO'], ['Y', 'YES']];
-
-for (var i = 0; i < questionsArray.length; i++) {
-  var answer = prompt(questionsArray[i]).toUpperCase();
-  if (answer === correctAnswers[i][0] || answer === correctAnswers[i][1]) {
-    alert(correctResponse[i]);
-    console.log(correctResponse[i]);
-    totalTally++;
-  }else{
-    alert(wrongResponse[i]);
-  }
-}
+// var questionsArray = ['Am I from Seattle?','Have I been skydiving before?',
+// 'My favorite place I\'ve recently visted is DC?','My favorite burger place is In\' N\' Out?','Have I studied abroad in Tahiti?'];
+//
+// var correctResponse = ['Correct! I am from Seattle','Correct! I am planning to!','Correct! My favorite landmark was the Jefferson Memorial.','You\'re right! But there has never been a burger I didn\'t like!','Correct! I have studied abroad in Tahiti!'];
+//
+// var wrongResponse = ['No! I am from Seattle!','No! But I plan to!','Wrong! It was my favorite recent visit!','No! But I really want to try it!','No! I did study abroad in Tahiti.'];
+//
+// var correctAnswers = [['Y', 'YES'], ['N', 'NO'], ['Y', 'YES'], ['N', 'NO'], ['Y', 'YES']];
+//
+// for (var i = 0; i < questionsArray.length; i++) {
+//   var answer = prompt(questionsArray[i]).toUpperCase();
+//   if (answer === correctAnswers[i][0] || answer === correctAnswers[i][1]) {
+//     alert(correctResponse[i]);
+//     console.log(correctResponse[i]);
+//     totalTally++;
+//   }else{
+//     alert(wrongResponse[i]);
+//   }
+// }
 
 // //User is asked to guess the age I am and summarizes how many guesses they've use.
-var ageGuesses = 1;
-var answer6 = parseInt(prompt('What is my age?'));
-
-if(answer6 === 23){
-  alert('Wow ' + userName + '! You\'re right, great guess!');
-}else{
-  while(ageGuesses <= 4 && answer6 != 23){
-    if(answer6 > 23){
-      answer6 = prompt('Sorry ' + userName + ', a little lower.');
-      ageGuesses++;
-    }else{
-      answer6 = prompt('Sorry ' + userName + ', a little higher.');
-      ageGuesses++;
-    }
-  }
-}
-
-if(ageGuesses < 4){
-  alert('You used ' + ageGuesses + ' out of your 4 guesses!');
-  totalTally++;
-}else{
-  alert('Sorry, you used all your guesses! My age is 23.');
-}
+// var ageGuesses = 1;
+// var answer6 = parseInt(prompt('What is my age?'));
+//
+// if(answer6 === 23){
+//   console.log('The user\'s guess was: ' + answer6);
+//   alert('Wow ' + userName + '! You\'re right, great guess!');
+// }else{
+//   while(ageGuesses <= 4 && answer6 != 23){
+//     if(answer6 > 23){
+//       console.log('The user\'s guess was: ' + answer6);
+//       answer6 = prompt('Sorry ' + userName + ', a little lower.');
+//       ageGuesses++;
+//     }else{
+//       console.log('The user\'s guess was: ' + answer6);
+//       answer6 = prompt('Sorry ' + userName + ', a little higher.');
+//       ageGuesses++;
+//     }
+//   }
+// }
+//
+// if(ageGuesses < 5){
+//   alert('Correct! You used ' + ageGuesses + ' out of your 4 guesses!');
+//   totalTally++;
+// }else{
+//   alert('Sorry, you used all your guesses! My age is 23.');
+// }
 
 //User is asked to guess a place I would like to visit and reports how many guesses they've used.
 var guessNumber = 1;
@@ -72,14 +62,15 @@ var answer7 = prompt('Can you guess a country I would like to visit?').toUpperCa
 while(guess === false && guessNumber < 6){
   for(var i = 0; i < 6; i++){
     if(answer7 === places[i]){
+      console.log('The user guessed a country correctly with: ' + answer7);
       alert('Correct ' + userName + '! You\'ve used ' + guessNumber + ' guesses.');
       guess = true;
       totalTally++;
       break;
-    }else{
     }
   }
   if(guess === false){
+    console.log(userName + ' guessed incorrectly with: ' + answer7);
     answer7 = prompt('Guess Again').toUpperCase();
     guessNumber++;
   }
